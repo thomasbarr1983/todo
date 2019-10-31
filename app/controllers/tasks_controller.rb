1ec4bff5ac7e77870d6a321f16575b5fb1a879bd
Thomas Barr <thomas.barr1983@gmail.com>
2019-10-31 23:59:11 +0000

    7 commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    render json: Task.all
+    render json: Task.order(:id)
   end
 
   def update
@@ -9,10 +9,10 @@ class TasksController < ApplicationController
     render json: task
   end
 
-
   private
 
   def task_params
     params.require(:task).permit(:done)
-  end  
+  end
+
 end
